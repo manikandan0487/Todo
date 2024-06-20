aa476d5fdf8c5b513ab48b0a702691652923ee4a
Manikandan <manimkd9677@gmail.com>
2024-06-20 17:21:11 +0530

    Touch Backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import ListTasks from "./components/ListTasks";
 import { Toaster } from 'react-hot-toast';
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import {TouchBackend} from 'react-dnd-touch-backend'
 
 function App() {
   const [tasks, setTasks] = useState([]);
   useEffect(() => {
     setTasks(JSON.parse(localStorage.getItem("todos")));
   }, []);
+  const Backend = TouchBackend || HTML5Backend;
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={Backend}>
      <Toaster 
       position="top-center"
     />
