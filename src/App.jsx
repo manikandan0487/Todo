d5cc6e2f9d7af466e28b0dc2dff9ad539cf3b7e3
Manikandan <manimkd9677@gmail.com>
2024-06-20 17:50:33 +0530

    react-responsive

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,11 @@ function App() {
     setTasks(JSON.parse(localStorage.getItem("todos")));
   }, []);
 
-  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
-  const Backend = isMobile ? TouchBackend : HTML5Backend;
-  const options = {
-    scrollAngleRanges: [
-      { start: 30, end: 150 },
-      { start: 210, end: 330 },
-    ],
-  };
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+  const Backend = isTabletOrMobile ? TouchBackend : HTML5Backend;
+
   return (
-    <DndProvider backend={Backend} options={options}>
+    <DndProvider backend={Backend}>
       <Toaster position="top-center" />
       <div className=" bg-slate-100 min-w-full  min-h-screen flex flex-col items-center  p-4 gap-10">
         <h1 className=" font-semibold text-2xl mt-16">TODO</h1>
