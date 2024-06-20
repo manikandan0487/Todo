46d3e1758f170faf6a1fe9ca9b4d7f21233a752e
Manikandan <manimkd9677@gmail.com>
2024-06-20 20:51:27 +0530

    touchBackend

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,21 @@ function App() {
 
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
   const Backend = isTabletOrMobile ? TouchBackend : HTML5Backend;
+  const isTouchDevice = () => {
+    if ("ontouchstart" in window) {
+      return true;
+    }
+    return false;
+  };
+  const options = { 
+    scrollAngleRanges: (sourceTypes) => {
+        if (sourceTypes.includes('SCROLLABLE_SOURCE')) return [{ start: 30, end: 150 }];
+        return undefined;
+    },
+}
 
   return (
-    <DndProvider backend={Backend}>
+    <DndProvider backend={Backend} options={isTabletOrMobile && isTouchDevice()  ? options : {}}>
       <Toaster position="top-center" />
       <div className=" bg-slate-100 min-w-full  min-h-screen flex flex-col items-center  p-4 gap-10">
         <h1 className=" font-semibold text-2xl mt-16">TODO</h1>
